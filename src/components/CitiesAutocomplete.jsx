a00b82bd4bea92b5e4b58a8432c58589400339d2
vcarrara <victor.carrara@utt.fr>
2020-05-23 11:46:44 +0200

    File separation and query string for requests

---
+++ src/components/CitiesAutocomplete.jsx
@@ -0,0 +1,80 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { Autocomplete } from 'react-materialize'
+
+class CitiesAutocomplete extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            cityInputValue: '',
+            currentCity: {},
+            citiesRawData: [],
+            autocompleteData: {},
+        }
+    }
+
+    /**
+     * When the component mounts, the system fetches one time (only one) french cities in geo.api.gouv.fr API
+     */
+    componentDidMount() {
+        this.fetchCities()
+    }
+
+    /**
+     * When a new value is entered in the autocomplete
+     */
+    handleAutocomplete = (value) => {
+        const city = this.state.citiesRawData.find((entry) => entry.nom === value)
+        this.updateParent(city)
+        this.setState({ cityInputValue: value })
+    }
+
+    /**
+     * If the autocomplete input matches a value in the raw datasource, the city is given back to the parent throughout a function
+     */
+    updateParent = (city) => {
+        if (this.props.onCityChange) {
+            this.props.onCityChange(city)
+        }
+    }
+
+    fetchCities = () => {
+        fetch(`https://geo.api.gouv.fr/communes?fields=nom,centre&format=json&geometry=centre`)
+            .then((x) => x.json())
+            .then((data) => {
+                /*
+                autocompleteData should be :
+                {
+                    firstItem: null,
+                    secondItem: null
+                },
+                */
+                const autocompleteData = data.reduce((previous, current) => {
+                    previous[current.nom] = null
+                    return previous
+                }, {})
+                this.setState({
+                    citiesRawData: data,
+                    autocompleteData: autocompleteData,
+                })
+            })
+    }
+
+    render() {
+        const options = {
+            data: this.state.autocompleteData,
+            limit: 10,
+            onAutocomplete: this.handleAutocomplete,
+        }
+        return (
+            <Autocomplete placeholder="Ville" options={options} onChange={(e) => this.handleAutocomplete(e.target.value)} value={this.state.cityInputValue} />
+        )
+    }
+}
+
+CitiesAutocomplete.propTypes = {
+    placeholder: PropTypes.string,
+    onCityChange: PropTypes.func,
+}
+
+export default CitiesAutocomplete
--- src/components/MainForm.jsx
+++ src/components/MainForm.jsx
@@ -1,90 +1,51 @@
 import React, { Component } from 'react'
 import Weather from 'components/Weather/Weather.jsx'
 import { Row, Button, Autocomplete } from 'react-materialize'
+import { withRouter } from 'react-router-dom'
+import queryString from 'query-string'
+import CitiesAutocomplete from 'components/CitiesAutocomplete.jsx'
 
 class MainForm extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            cityInputValue: '',
-            currentCity: {},
-            citiesRawData: [],
-            autocompleteOption: {},
+            city: undefined,
         }
     }
 
-    /**
-     * When the component mounts, the system fetches one time (only one) french cities in geo.api.gouv.fr API
-     */
-    componentDidMount() {
-        this.fetchCities()
-    }
-
     handleClick = () => {
-        const coordinates = this.state.citiesRawData.find((entry) => entry.nom === this.state.cityInputValue).centre.coordinates
-        this.setState({
-            currentCity: {
-                nom: this.state.cityInputValue,
-                lat: coordinates[1],
-                lon: coordinates[0],
-            },
+        let query = {}
+        if (this.state.city) {
+            query['nom'] = this.state.city.nom
+            query['lat'] = this.state.city.centre.coordinates[1]
+            query['lon'] = this.state.city.centre.coordinates[0]
+        }
+        this.props.history.push({
+            pathname: '/',
+            search: queryString.stringify(query),
         })
     }
 
-    handleAutocomplete = (value) => {
-        this.setState({ cityInputValue: value })
-    }
-
-    fetchCities = () => {
-        fetch(`https://geo.api.gouv.fr/communes?fields=nom,centre&format=json&geometry=centre`)
-            .then((x) => x.json())
-            .then((data) => {
-                /*
-                autocompleteOption should be :
-                {
-                    data: {
-                        firstItem: null,
-                        secondItem: null
-                    },
-                    limit: <any number>,
-                    onAutocomplete: <a function>,
-                }
-                */
-                const autocompleteData = data.reduce((previous, current) => {
-                    previous[current.nom] = null
-                    return previous
-                }, {})
-                this.setState({
-                    citiesRawData: data,
-                    autocompleteOption: {
-                        data: autocompleteData,
-                        limit: 10,
-                        onAutocomplete: this.handleAutocomplete,
-                    },
-                })
-            })
+    handleCityChange = (city) => {
+        this.setState({ city: city })
     }
 
     render() {
+        const query = queryString.parse(this.props.location.search)
         return (
             <>
                 <Row>
-                    <Autocomplete
-                        placeholder="Ville"
-                        options={this.state.autocompleteOption}
-                        onChange={(e) => this.handleAutocomplete(e.target.value)}
-                        value={this.state.cityInputValue}
-                    />
+                    <CitiesAutocomplete onCityChange={this.handleCityChange} />
                 </Row>
                 <Row>
                     <Button node="button" onClick={this.handleClick} waves="light">
                         Rechercher
                     </Button>
                 </Row>
-                <Row>{this.state.currentCity.nom && <Weather nom={this.state.currentCity.nom} lat={this.state.currentCity.lat} lon={this.state.currentCity.lon} />}</Row>
+                <Row>{query.lat && query.lon && query.nom && <Weather nom={query.nom} lat={Number(query.lat)} lon={Number(query.lon)} />}</Row>
             </>
         )
     }
 }
 
-export default MainForm
+export default withRouter(MainForm)
