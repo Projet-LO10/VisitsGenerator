f64ec1eaf4ac8e13d37e13f3062567af52489e54
Damien71 <50024514+Damien71@users.noreply.github.com>
2020-06-11 15:42:59 +0200

    Calcul automatique du rejet carbone

--- client/src/components/Visite/Visite.jsx
+++ client/src/components/Visite/Visite.jsx
@@ -53,7 +53,7 @@ class Visite extends Component {
         return (
             <div>
                 {vehicle && <Vehicles car={vehicle} />}
-                {vehicle && <Ecology dist={"12"} car={vehicle} />}
+                {vehicle && <Ecology dist={roads.roads.distance.value} car={vehicle} />}
                 <Weather dataSource={weather} ville={ville} date={date} />
                 <Museums dataSource={museums} ville={ville} />
                 <HistoricalMonuments dataSource={monuments} ville={ville} />
