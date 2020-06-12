83334662fe1d2f83032bb33687d617d96cc0f7a7
Axel Garnier <axel.garnier@utt.fr>
2020-06-12 23:45:01 +0200

    css mineur

--- client/src/components/Weather/Weather.jsx
+++ client/src/components/Weather/Weather.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import { Card } from 'react-materialize'
+import { Row, Col, Card } from 'react-materialize'
 
 class Weather extends Component {
     constructor(props) {
@@ -51,25 +51,29 @@ class Weather extends Component {
             <div>
                 <h4>Voici les données météo à {ville}</h4>
 
-                {/*CARD Générale ------------------------------------------*/}
-                <Card className="center">
-                    <span className="card-title">Informations générales</span>
-                    <h6>Ville : {ville}</h6>
-                    <h6>Timezone : {dataSource.timezone}</h6>
-                    <h6>Pays : {dataSource.country_code}</h6>
-                </Card>
-
-                {/*CARD pour le jour demandé ------------------------------------------*/}
-                <Card className="center">
-                    <div className="card-image" style={{ width: '10%', margin: 'auto' }}>
-                        {/* <img src={'src/images/weather/' + dataSource['weather']['icon'] + '.png'}></img> */}
-                        <img src={`/images/weather/${dataSource['weather']['icon']}.png`} />
-                    </div>
-                    <span className="card-title">Météo le {date}</span>
-                    <h6>Température : {dataSource['temp']} degrés</h6>
-                    <h6>Description : {dataSource['weather']['description']}</h6>
-                </Card>
-
+                <Row>
+                  <Col l={6}>
+                  {/*CARD Générale ------------------------------------------*/}
+                    <Card className="center card-meteo">
+                        <span className="card-title">Informations générales</span>
+                        <h6>Ville : {ville}</h6>
+                        <h6>Timezone : {dataSource.timezone}</h6>
+                        <h6>Pays : {dataSource.country_code}</h6>
+                    </Card>
+                  </Col>
+                  <Col l={6}>
+                    {/*CARD pour le jour demandé ------------------------------------------*/}
+                    <Card className="center">
+                        <div className="card-image" style={{ width: '10%', margin: 'auto' }}>
+                            {/* <img src={'src/images/weather/' + dataSource['weather']['icon'] + '.png'}></img> */}
+                            <img src={`/images/weather/${dataSource['weather']['icon']}.png`} />
+                        </div>
+                        <span className="card-title">Météo le {date}</span>
+                        <h6>Température : {dataSource['temp']} degrés</h6>
+                        <h6>Description : {dataSource['weather']['description']}</h6>
+                    </Card>
+                  </Col>
+                </Row>
                 {/*CARD Générale ------------------------------------------*/}
                 {/* <Card className="center blue-grey darken-1 orange-text">
                     <span className="card-title">Informations générales</span>
