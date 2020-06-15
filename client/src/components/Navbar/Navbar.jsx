c209fc549fdf36d721ed3b12118278d3d9cd5286
vcarrara <victor.carrara@utt.fr>
2020-06-15 16:46:28 +0200

    CSS Fix

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 
 const Navbar = () => (
     <nav>
-        <div className="nav-wrapper blue darken-4">
+        <div className="nav-wrapper blue darken-3">
             <a href="#" className="brand-logo center">
                 Générateur de visites guidées
             </a>
